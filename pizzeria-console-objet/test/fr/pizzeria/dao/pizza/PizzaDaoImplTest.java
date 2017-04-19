acd3c8f5d60170756251855a87022e5da3a5727b
Estelle PELLE <estellepelle9261@gmail.com>
2017-04-19 11:01:50 +0200

    valide avec des tests unitaires

---
+++ pizzeria-console-objet/test/fr/pizzeria/dao/pizza/PizzaDaoImplTest.java
@@ -0,0 +1,30 @@
+package fr.pizzeria.dao.pizza;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import fr.pizzeria.model.Pizza;
+
+
+public class PizzaDaoImplTest {
+
+	@Test
+	public void test_findAllPizzas_initialisation(){
+		
+		List<Pizza> pizza = new ArrayList<>();
+		PizzaDaoImpl pizzas = new PizzaDaoImpl(pizza);
+		
+		//invocation de la méthode
+		List<Pizza >resultat = pizzas.findAllPizzas();
+		
+		Assert.assertNotNull("La liste n'est pas null", resultat);
+		
+	}
+	
+	
+}
